0687622115ad7e1e296de4cbe76c7be84cd72777
CuongDao160507 <cuongdaoviet1605@gmail.com>
2026-01-14 15:16:42 +0700

    fix axios

--- training/mern-form-builder/client/src/api/axiosClient.js
+++ training/mern-form-builder/client/src/api/axiosClient.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Dùng biến môi trường để linh hoạt (Local thì localhost, lên mạng thì dùng link Render)
+// Hoặc paste thẳng link Render vào đây nếu lười cấu hình
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5000/api', // Đường dẫn gốc của Server
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
